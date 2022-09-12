d667caa809185b3083b5bb484a330c5dc534da57
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-09-12 19:07:07 +0300

    basic server config + .env file

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(cors())
+
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, async () => {
+    console.log(`server open at http://localhost:${port}`);
+})
